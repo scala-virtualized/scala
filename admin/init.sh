5a0946477f293210184b7d44a5935d23f9255674
Adriaan Moors <adriaan@lightbend.com>
2018-02-26 14:28:12 +0100

    wip: build release using Travis

---
+++ admin/init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sensitive() {
+  perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/credentials-private-repo > ~/.credentials-private-repo
+  perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/credentials-sonatype     > ~/.credentials-sonatype
+  perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/sonatype-curl            > ~/.sonatype-curl
+  # perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/m2-settings.xml          > ~/.m2/settings.xml  -- not needed anymore (used for ide integration?)
+
+  openssl aes-256-cbc -K $encrypted_40abf89dbafd_key -iv $encrypted_40abf89dbafd_iv -in files/gpg_subkey.enc | gpg --import
+}
+
+# directories needed by sensitive part
+# mkdir -p ~/.m2 -- not needed anymore (used for ide integration?)
+mkdir -p ~/.ssh
+
+# don't let anything escape from the sensitive part (e.g. leak environment var by echoing to log on failure)
+sensitive >/dev/null 2>&1
+
+mkdir -p ~/.sbt/0.13/plugins
+cp files/gpg.sbt ~/.sbt/0.13/plugins/
+
+
+# just to verify
+gpg --list-keys
+
+export SBT_CMD=$(which sbt)
