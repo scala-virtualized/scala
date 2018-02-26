d4d2e4c1d66a38663884d9e052118570b6886db8
Adriaan Moors <adriaan@lightbend.com>
2018-02-26 15:18:10 +0100

    gpg

--- admin/init.sh
+++ admin/init.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-PRIV_KEY_SECRET="[secure]"
-PRIVATE_REPO_PASS="[secure]"
-SONA_USER="[secure]"
-SONA_PASS="[secure]"
 
 sensitive() {
   perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/credentials-private-repo > ~/.credentials-private-repo
@@ -11,7 +7,7 @@ sensitive() {
   perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/sonatype-curl            > ~/.sonatype-curl
   # perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg' < files/m2-settings.xml          > ~/.m2/settings.xml  -- not needed anymore (used for ide integration?)
 
-  openssl aes-256-cbc -d -K $encrypted_40abf89dbafd_key -iv $encrypted_40abf89dbafd_iv -in files/bla.enc # | gpg --import
+  openssl aes-256-cbc -d -K $encrypted_40abf89dbafd_key -iv $encrypted_40abf89dbafd_iv -in files/gpg_subkey.enc | gpg --import
 }
 
 # directories needed by sensitive part
@@ -19,18 +15,14 @@ sensitive() {
 mkdir -p ~/.ssh
 
 # don't let anything escape from the sensitive part (e.g. leak environment var by echoing to log on failure)
-sensitive #>/dev/null 2>&1
+sensitive >/dev/null 2>&1
 
 # just to verify
-cat ~/.credentials* ~/.sonatype-curl
-
 gpg --list-keys
-
 
 
 mkdir -p ~/.sbt/0.13/plugins
 cp files/gpg.sbt ~/.sbt/0.13/plugins/
-
 
 export SBT_CMD=$(which sbt)
 
